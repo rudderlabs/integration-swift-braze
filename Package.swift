d920344372263bf083e5ee97c8ae5fd7f8def8ac
Vishal Gupta <vguptakota1998@gmail.com>
2025-11-20 16:03:24 +0530

    chore: add braze and swift sdk deps
    Scanned-by: gitleaks 8.29.0

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "integration-swift-braze",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +18,20 @@ let package = Package(
             targets: ["RudderIntegrationBraze"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/braze-inc/braze-swift-sdk-prebuilt-static", .upToNextMajor(from: "12.0.0")),
+        // TODO: Update rudder-sdk-swift dependency after the stable release of rudder-sdk-swift.
+        .package(url: "https://github.com/rudderlabs/rudder-sdk-swift.git", branch: "feat/sdk-502-make-standard-integration-public")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RudderIntegrationBraze"
+            name: "RudderIntegrationBraze",
+            dependencies: [
+                .product(name: "BrazeKit", package: "braze-swift-sdk-prebuilt-static"),
+                .product(name: "RudderStackAnalytics", package: "rudder-sdk-swift")
+            ]
         ),
         .testTarget(
             name: "RudderIntegrationBrazeTests",
