f14bb7c9ccd7f38e5cae77a7ba41c21354615f5e
Vishal Gupta <vguptakota1998@gmail.com>
2025-11-21 17:23:13 +0530

    feat: add basic example app setup
    Scanned-by: gitleaks 8.29.0

---
+++ Example/BrazeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrazeExampleApp.swift
+//  BrazeExample
+//
+//  Created by Vishal Gupta on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BrazeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
